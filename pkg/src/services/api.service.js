1a0b9d08145fef335d8d5a68cbc625dc4666ba8d
Fragmnt X <fragmnt@x.com>
2020-06-17 20:57:11 -0400

    Changed up!

--- pkg/src/services/api.service.js
+++ pkg/src/services/api.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://gdx3r.top/'
+const API_BASE_URL = 'https://gdx3r.top'
 
 export const addParcel = (pk) => {
     const payload = pk.toString();
